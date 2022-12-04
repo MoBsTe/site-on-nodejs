dfcf58392024e04346aa1a44d1f3eafffc31ec63
MoBsTe <valery.lobach@gail.com>
2022-12-05 00:25:46 +0100

    add file,folder

---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+
+
+axios.get('/url')
+    .then((response) => {
+
+        let products = response.data;
+
+        let main_items = document.querySelector("div.main_items")
+        for (let i = 0; i < products.length; i++) {
+            // создаем элимент div
+            let div = document.createElement("div");
+            // вставляемего внуьрб мейна 
+            div.classList.add("main_item");
+            div.innerHTML = products[i];
+            main_items.appendChild(div);
+        }
+    });
+
+
